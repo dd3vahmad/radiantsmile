bdc3e8752931fcea3c504306ccf3a457def84dd2
Rabiu Ahmad O. <rabiu2993@gmail.com>
2025-06-07 13:18:36 +0100

    Fix: Assets

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,18 +1,15 @@
 import Image from "next/image";
-import radiantSmileLogo from "@/asset/logo.jpg";
+import radiantSmileLogo from "@/asset/logo.png";
 
 const Navbar = () => {
   return (
-    <div className="w-full bg-white py-4 px-5">
-      {/* <Image
+    <div className="w-full bg-white py-4 px-5 flex items-center justify-between">
+      <Image
         src={radiantSmileLogo.src}
-        height={radiantSmileLogo.height * 0.3}
         width={100}
+        height={100}
         alt="Radiant Smile Logo"
-      /> */}
-      <div className="border overflow-hidden rounded border-blue-300 w-fit text-lg">
-        RadiantSmile
-      </div>
+      />
 
       <div className="items-center gap-2 hidden md:flex">{/* Yooooo */}</div>
 

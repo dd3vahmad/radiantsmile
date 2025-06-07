3db11121d6799648bf0cf77c09be0dfdacef839f
Rabiu Ahmad O. <rabiu2993@gmail.com>
2025-06-07 13:44:48 +0100

    Add: Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,19 +1,25 @@
 import Image from "next/image";
 import radiantSmileLogo from "@/asset/logo.png";
+import { Navitems } from "./Navitems";
+import { Button } from "./ui/button";
 
 const Navbar = () => {
   return (
-    <div className="w-full bg-white py-4 px-5 flex items-center justify-between">
-      <Image
-        src={radiantSmileLogo.src}
-        width={100}
-        height={100}
-        alt="Radiant Smile Logo"
-      />
+    <div className="w-full bg-white">
+      <div className="w-full max-w-7xl mx-auto py-4 px-5 flex items-center justify-between">
+        <Image
+          src={radiantSmileLogo.src}
+          width={100}
+          height={100}
+          alt="Radiant Smile Logo"
+        />
 
-      <div className="items-center gap-2 hidden md:flex">{/* Yooooo */}</div>
+        <Navitems />
 
-      <div></div>
+        <Button className="bg-blue-500 hover:bg-blue-500 hover:opacity-90 rounded-full cursor-pointer md:block hidden">
+          Book An Appointment
+        </Button>
+      </div>
     </div>
   );
 };
